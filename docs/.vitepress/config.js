00b8fbdf174cf4d606524b478e78e58f76f8db1f
Viktor Persson <viktor.persson@arcsin.se>
2022-10-25 21:06:43 +0200

    Add trailing slash to links.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,11 +15,11 @@ const communityMenu = {
 };
 
 const topNavItems = [
-  { text: 'Getting started', link: '/info/getting-started' },
-  { text: 'Downloads', link: '/info/downloads' },
-  { text: 'Documentation', link: '/docs' },
-  { text: 'Screenshots', link: '/info/screenshots' },
-  { text: 'About', link: '/info/about' },
+  { text: 'Getting started', link: '/info/getting-started/' },
+  { text: 'Downloads', link: '/info/downloads/' },
+  { text: 'Documentation', link: '/docs/' },
+  { text: 'Screenshots', link: '/info/screenshots/' },
+  { text: 'About', link: '/info/about/' },
 ];
 
 export default defineConfig({
@@ -55,64 +55,64 @@ export default defineConfig({
         '/docs': [
           {
             items: [
-              { text: 'Documentation index', link: '/docs' },
-              { text: 'Frequently Asked Questions', link: '/docs/faq' },
-              { text: 'File and directory structure', link: '/docs/structure' },
+              { text: 'Documentation index', link: '/docs/' },
+              { text: 'Frequently Asked Questions', link: '/docs/faq/' },
+              { text: 'File and directory structure', link: '/docs/structure/' },
             ]
           },
           {
             text: 'Features',
             items: [
-              { text: 'Independent Physics', link: '/docs/independent-physics' },
-              { text: 'Message Filtering', link: '/docs/message-filtering' },
-              { text: 'Multiview', link: '/docs/multiview' },
-              { text: 'Server Browser', link: '/docs/server-browser' },
-              { text: 'Scripting', link: '/docs/scripting' },
-              { text: 'Teamplay Communication', link: '/docs/teamplay-communication' },
-              { text: 'Video Capture', link: '/docs/video-capture' },
-              { text: 'Voice Support', link: '/docs/voice-support' },
-              { text: 'Weapon Scripts', link: '/docs/weapon-scripts' },
+              { text: 'Independent Physics', link: '/docs/independent-physics/' },
+              { text: 'Message Filtering', link: '/docs/message-filtering/' },
+              { text: 'Multiview', link: '/docs/multiview/' },
+              { text: 'Server Browser', link: '/docs/server-browser/' },
+              { text: 'Scripting', link: '/docs/scripting/' },
+              { text: 'Teamplay Communication', link: '/docs/teamplay-communication/' },
+              { text: 'Video Capture', link: '/docs/video-capture/' },
+              { text: 'Voice Support', link: '/docs/voice-support/' },
+              { text: 'Weapon Scripts', link: '/docs/weapon-scripts/' },
             ]
           },
           {
             text: 'Graphics',
             items: [
-              { text: 'Crosshairs', link: '/docs/crosshairs' },
-              { text: 'Charsets', link: '/docs/charsets' },
-              { text: 'Fakeshaft', link: '/docs/fakeshaft' },
-              { text: 'Frag Tracker', link: '/docs/frag-tracker' },
-              { text: 'Heads Up Display (HUD)', link: '/docs/hud' },
-              { text: 'Particles', link: '/docs/particles' },
-              { text: 'Player Skins', link: '/docs/player-skins' },
-              { text: 'Textures', link: '/docs/textures' },
+              { text: 'Crosshairs', link: '/docs/crosshairs/' },
+              { text: 'Charsets', link: '/docs/charsets/' },
+              { text: 'Fakeshaft', link: '/docs/fakeshaft/' },
+              { text: 'Frag Tracker', link: '/docs/frag-tracker/' },
+              { text: 'Heads Up Display (HUD)', link: '/docs/hud/' },
+              { text: 'Particles', link: '/docs/particles/' },
+              { text: 'Player Skins', link: '/docs/player-skins/' },
+              { text: 'Textures', link: '/docs/textures/' },
             ]
           },
           {
             text: 'Reference',
             items: [
-              { text: 'Commands', link: '/docs/commands' },
-              { text: 'Command Line Parameters', link: '/docs/command-line-parameters' },
-              { text: 'Macros', link: '/docs/macros' },
+              { text: 'Commands', link: '/docs/commands/' },
+              { text: 'Command Line Parameters', link: '/docs/command-line-parameters/' },
+              { text: 'Macros', link: '/docs/macros/' },
             ]
           },
           {
             text: 'Settings reference',
             items: [
-              { text: 'Demos', link: '/docs/settings/demos' },
-              { text: 'Graphics ', link: '/docs/settings/graphics' },
-              { text: 'HUD', link: '/docs/settings/hud' },
-              { text: 'Input', link: '/docs/settings/input' },
-              { text: 'Miscellaneous', link: '/docs/settings/miscellaneous' },
-              { text: 'Multiplayer', link: '/docs/settings/multiplayer' },
-              { text: 'Server', link: '/docs/settings/server' },
-              { text: 'Sound', link: '/docs/settings/sound' },
-              { text: 'Teamplay', link: '/docs/settings/teamplay' },
+              { text: 'Demos', link: '/docs/settings/demos/' },
+              { text: 'Graphics ', link: '/docs/settings/graphics/' },
+              { text: 'HUD', link: '/docs/settings/hud/' },
+              { text: 'Input', link: '/docs/settings/input/' },
+              { text: 'Miscellaneous', link: '/docs/settings/miscellaneous/' },
+              { text: 'Multiplayer', link: '/docs/settings/multiplayer/' },
+              { text: 'Server', link: '/docs/settings/server/' },
+              { text: 'Sound', link: '/docs/settings/sound/' },
+              { text: 'Teamplay', link: '/docs/settings/teamplay/' },
             ]
           },
           {
             text: 'Misc',
             items: [
-              { text: 'Upgrading', link: '/docs/upgrading' },
+              { text: 'Upgrading', link: '/docs/upgrading/' },
             ]
           },
           communityMenu
