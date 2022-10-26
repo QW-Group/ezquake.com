82d6965f070700e2803789d53a5aed31626243e4
Viktor Persson <viktor.persson@arcsin.se>
2022-10-26 16:11:45 +0200

    Update documentation overview.

---
+++ docs/components/DocumentationOverview.vue
@@ -0,0 +1,54 @@
+<script>
+
+import menus from './../.vitepress/menus';
+
+export default {
+  data() {
+    return {
+      menus: [
+        menus.features,
+        menus.graphics,
+        menus.reference,
+        menus.settingsReference,
+        menus.misc,
+        menus.sourceCode,
+      ]
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <div class="container">
+    <div class="column" v-for="menu in menus">
+      <h2>{{ menu.text }}</h2>
+      <ul>
+        <li v-for="item in menu.items">
+          <a :href="item.link">{{ item.text }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-column-gap: 20px;
+}
+
+@media (min-width: 640px) and (max-width: 1023px) {
+  .container {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 1024px) {
+  .container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+</style>
